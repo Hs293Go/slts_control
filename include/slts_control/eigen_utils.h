1dce922d869db3fbaf57400fb1658070d151a88b
H S Helson Go <hellston20a@gmail.com>
2022-06-16 18:02:59 -0400

    Design Eigen debug macros

--- include/slts_control/eigen_utils.h
+++ include/slts_control/eigen_utils.h
@@ -10,6 +10,19 @@
 
 #define ZEROS(r, c) (Eigen::Matrix<double, r, c>::Zero())
 
+#ifndef NDEBUG
+#include <iostream>
+#define DBG_MAT_FMT \
+  (Eigen::IOFormat(Eigen::StreamPrecision, 0, ",", ";\n", "", "", "[", "]"))
+
+#define DBG_MAT_STREAM(stream, mat)                                      \
+  do {                                                                   \
+    stream << "Value of " #mat ":\n" << mat.format(DBG_MAT_FMT) << "\n"; \
+  } while (0)
+
+#define DBG_MAT(mat) DBG_MAT_STREAM(std::cout, mat);
+#endif
+
 namespace utils {
 
 template <typename Derived1, typename Derived2, typename Derived3>
