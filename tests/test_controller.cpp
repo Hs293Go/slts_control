ac56fdc104a63f0066ea4844c78140d9508418f5
H S Helson Go <hellston20a@gmail.com>
2022-06-17 16:14:09 -0400

    Initialize tracker in test
    Also makes dataset public for access in tests

--- tests/test_controller.cpp
+++ tests/test_controller.cpp
@@ -12,7 +12,16 @@
 #include <mutex>
 #include <unordered_map>
 
+#include "slts_control/common.h"
+#include "slts_control/robust_tracker.h"
+
 class TestController : public ::testing::Test {
+ public:
+  TestController() : tracker(Properties()) {}
+
+  std::unordered_map<std::string, Eigen::MatrixXd> dataset;
+  control::RobustTracker tracker;
+
  private:
   void readFromFile() {
     auto file = std::filesystem::path(__FILE__).replace_extension(".json");
@@ -25,13 +34,22 @@ class TestController : public ::testing::Test {
       const auto& it = root[key];
       const auto& sz = it["size"];
       const auto& val = it["value"];
-      dataset_.emplace(key, Eigen::MatrixXd(sz[0].asInt(), sz[1].asInt()));
-      std::transform(val.begin(), val.end(), dataset_[key].data(),
+      dataset.emplace(key, Eigen::MatrixXd(sz[0].asInt(), sz[1].asInt()));
+      std::transform(val.begin(), val.end(), dataset[key].data(),
                      std::mem_fn(&Json::Value::asDouble));
     }
   }
 
-  std::unordered_map<std::string, Eigen::MatrixXd> dataset_;
+  static common::SLTSProperty Properties() {
+    common::SLTSProperty p;
+    p.cable_length = 0.98;
+    p.uav_mass = 1.63;
+    p.pld_mass = 0.5;
+    return p;
+  }
 };
 
-int main(int argc, char** argv) {}
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
