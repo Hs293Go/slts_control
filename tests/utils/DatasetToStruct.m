c45864c44f68b1405332e6af1ff6fccb556d94eb
H S Helson Go <hellston20a@gmail.com>
2022-06-18 02:23:52 -0400

    Fix dimension handling mistake

--- tests/utils/DatasetToStruct.m
+++ tests/utils/DatasetToStruct.m
@@ -3,7 +3,10 @@ function json = DatasetToStruct(ds, fields)
     yout = ds.yout;
     json = struct('tout', struct('size', size(tout), 'value', tout));
     for it = fields
-        val = squeeze(yout.get(it).Values.Data).';
+        val = squeeze(yout.get(it).Values.Data);
+        if isrow(val)
+            val = val.';
+        end
         sz = size(val);
         val = reshape(val, [], 1);
         json.(it) = struct('size', sz, 'value', val);
